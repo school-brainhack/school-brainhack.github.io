9b8a2a6de5d06c1dbcfea74999edb84aa80a6e76
plopezmaggi <133046724+plopezmaggi@users.noreply.github.com>
2023-06-09 23:50:51 -0300

    Add files via upload

--- content/en/project/Mice-Behavioral-Analysis/Motion_Tracking/utils.py
+++ content/en/project/Mice-Behavioral-Analysis/Motion_Tracking/utils.py
@@ -1,46 +1,39 @@
 import pandas as pd
 import numpy as np
 
-def suavizar(vec, n):
-    kernel = [1 / n] * n
-    return np.convolve(vec, kernel, "same")
-
-class Punto:
-    def __init__(self, df, tabla):
-        data = df[tabla]
+class Point:
+    def __init__(self, df, table):
+        data = df[table]
         
-        x = suavizar(data['x'], 5)
-        y = suavizar(data['y'], 5)
+        x = data['x']
+        y = data['y']
         
-        self.posiciones = np.dstack((x, y))[0]
-        
-        
+        self.positions = np.dstack((x, y))[0]
     
     @staticmethod
     def dist(p1, p2):
-        return np.linalg.norm(p1.posiciones - p2.posiciones, axis=1)
+        return np.linalg.norm(p1.positions - p2.positions, axis=1)
         
-class Obj(Punto):
-    def __init__(self, df, tabla):
-        super(Obj, self).__init__(df, tabla)
+class Obj(Point):
+    def __init__(self, df, table):
+        super(Obj, self).__init__(df, table)
         
-        self.posiciones = np.repeat(
-            np.expand_dims(np.mean(self.posiciones, axis=0), axis=0),
-            len(self.posiciones),
+        self.median_array = np.repeat(
+            np.expand_dims(np.median(self.positions, axis=0), axis=0),
+            len(self.positions),
             axis=0
         )
         
-class PuntoCuerpo(Punto):
-    pass
+        self.median_coordinates = self.median_array[0]
 
 class Vector():
-    def __init__(self, p1, p2, normalizar=True):
-        self.posiciones = p2.posiciones - p1.posiciones
+    def __init__(self, p1, p2, normalize=True):
+        self.positions = p2.positions - p1.positions
         
-        self.norm = np.linalg.norm(self.posiciones, axis=1)
+        self.norm = np.linalg.norm(self.positions, axis=1)
         
-        if normalizar:
-            self.posiciones = self.posiciones / np.repeat(
+        if normalize:
+            self.positions = self.positions / np.repeat(
                 np.expand_dims(
                     self.norm,
                     axis=1
@@ -48,13 +41,20 @@ class Vector():
                 2,
                 axis=1
             )
-            
+    
     @staticmethod
-    def cosine(v1, v2):
-        length = len(v1.posiciones)
-        cos = np.zeros(length)
-
+    def angle(v1, v2):
+        length = len(v1.positions)
+        
+        angle = np.zeros(length)
+        
         for i in range(length):
-            cos[i] = np.dot(v1.posiciones[i], v2.posiciones[i])
+            angle[i] = np.rad2deg(
+                np.arccos(
+                    np.dot(
+                        v1.positions[i], v2.positions[i]
+                    )
+                )
+            )
             
-        return cos
+        return angle
